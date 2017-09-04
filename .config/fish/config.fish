c305c946deac79d568d8d1b4eb5556d4ce4013ab
Nathalie Tate <nathalie.tate00@gmail.com>
2017-09-04 12:28:49 -0400

    BROWSER env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 set fish_greeting "Hi mom <3<3<3"
 set -x PANEL_FIFO /tmp/panel-fifo
 set -x EDITOR nvim
+set -x BROWSER qutebrowser
 set -x EMAIL nrt@nathalietate.xyz
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 
